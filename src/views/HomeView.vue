b17b0ad441b87e5c1b5caa89fb13f5da51e5b462
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-18 03:14:46 -0300

    Criando a NavBar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,29 @@
 <template>
-  <hello-world />
+  <v-app>
+    <NavBar class="index" />
+    <DashBoard class="index" />
+    <FooterView class="index" />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import NavBar from "../components/NavBar";
+import DashBoard from "../components/DashBoard";
+import FooterView from "../components/FooterView";
 
 export default {
   name: "HomeView",
 
   components: {
-    HelloWorld,
+    NavBar,
+    DashBoard,
+    FooterView,
   },
 };
 </script>
+
+<style scoped>
+.index {
+  z-index: 9999;
+}
+</style>
